a3b12bc94d1cc3fc9f1e6509122bc14a279f52f1
Isabel <isabelkaikkonen@gmail.com>
2024-08-04 13:22:09 +0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  //const url ='https://api.openweathermap.org/data/2.5/weather?lat=34.098907&lon=-118.327759&appid=863a098076fa97462d03f8ffc0e62315'
+
+
   return (
     <div className="App">
       <h1>Weather App</h1>
