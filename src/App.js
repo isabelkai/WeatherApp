55ace401bbf6fa799a980d16e346fecf4821bc98
Isabel <isabelkaikkonen@gmail.com>
2024-08-19 16:00:36 +0300

    Location search

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWater, faWind, faMagnifyingGlass, faTemperatureThreeQuarters } from '@fortawesome/free-solid-svg-icons'
+import { faWater, faWind, faTemperatureThreeQuarters } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
   const apiKey = process.env.REACT_APP_API_KEY;
-  //const url ='https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}';
+
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+      axios.get(url).then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      setLocation('');
+    }
+  }
 
 
   return (
-    <div className="app">
-      {/* <div className="container">
-        <div className="top">
-          <div className="input-container">
-            <input class="search-input" id="searchInput" placeholder="Enter location"/>
-            <button class="search-button" id="searchButton">
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </button>
-          </div>
-        </div> */}
+    <div className="app"> 
       <div className="weather-container">
         <div className="top">
+          <div className="search">
+            <input 
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter location'
+            type="text"/>
+          </div>
           <div className="location">
             <p>London</p>
           </div>
@@ -33,19 +46,19 @@ function App() {
           </div>
         </div>
         <div className="bottom">
-          <div className="feels">
+          <div className="detail">
             <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
-            <p className="bold">25°C</p>
+            <p className="value">25°C</p>
             <p>Feels like</p>
           </div>
-          <div className="humidity">
+          <div className="detail">
             <FontAwesomeIcon icon={faWater} />
-            <p className="bold">20%</p>
+            <p className="value">20%</p>
             <p>Humidity</p>
           </div>
-          <div className="wind">
+          <div className="detail">
             <FontAwesomeIcon icon={faWind} />
-            <p className="bold">5km/h</p>
+            <p className="value">5km/h</p>
             <p>Wind speed</p>
           </div>
         </div>
